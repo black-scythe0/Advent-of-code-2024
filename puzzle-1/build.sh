e0c57a7e7597a5d4932c4f756ce238903d8d28a5
black-scythe0 <scytheblack991@gmail.com>
2025-06-18 02:01:41 +0530

    puzzle-1

---
+++ puzzle-1/build.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+exe_name='puzzle-AOC-2024-1'
+
+
+rustc -o ${exe_name} ${exe_name}.rs &&
+
+mv ${exe_name} ~/exes/ &&
+
+chmod +x ~/exes/${exe_name} &&
+
+~/exes/${exe_name}
+
+
+echo " "
+echo "compilation complete exit with $(echo $?)"
+
+
+
+
+
+
+
+
---
+++ puzzle-1/puzzle-AOC-2024-1.rs
@@ -0,0 +1,16 @@
+use std::fs;
+//use std::env;
+//use std::any::type_name;
+fn main(){
+   //let mut  list_left = [0];
+   //let mut list_right = [0];
+   //list_left.sort();
+   //println!("sorted list: {:?}", list_left);
+   //
+   //let content = fs::read_to_string("AOC-2024-1.txt").expect("file must have been read"); 
+   //println!("file content:\n {content}");
+
+
+} 
+
+
