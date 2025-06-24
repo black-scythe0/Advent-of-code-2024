10dc22b614998ffb2e1afb406a83145e81480809
black-scythe0 <scytheblack991@gmail.com>
2025-06-25 01:35:34 +0530

    just a commit

--- puzzle-1/puzzle-AOC-2024-1.rs
+++ puzzle-1/puzzle-AOC-2024-1.rs
@@ -1,4 +1,7 @@
 use std::fs;
+//use std::str::FromStr;
+//use std::str::Split;
+
 //use std::env;
 //use std::any::type_name;
 fn main(){
@@ -7,8 +10,17 @@ fn main(){
    //list_left.sort();
    //println!("sorted list: {:?}", list_left);
    //
-   //let content = fs::read_to_string("AOC-2024-1.txt").expect("file must have been read"); 
-   //println!("file content:\n {content}");
+   let content: &str = &fs::read_to_string("AOC-2024-1.txt").expect("file must have been read");
+   let splited = content.split_terminator("\n");
+   //let splited = splited.split(" ");
+   let mut list = vec![0];
+   for i in splited{
+       list.push(i.parse::<i64>());
+       println!("{}", i);
+       break;
+
+   }
+   //println!("file content:\n {:#?}", splited);
 
 
 } 
